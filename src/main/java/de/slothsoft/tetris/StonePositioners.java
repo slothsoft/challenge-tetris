7b820779efc34c0572ed98a8bbaa3a401cdceae7
Slothsoft <admin@slothsoft.de>
2019-03-03 13:24:13 +0100

    #7 "fixed" tests (with removing them)

--- src/main/java/de/slothsoft/tetris/StonePositioners.java
+++ src/main/java/de/slothsoft/tetris/StonePositioners.java
@@ -1,121 +1,27 @@
 package de.slothsoft.tetris;
 
-import java.io.IOException;
-import java.lang.reflect.Modifier;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 
+import de.slothsoft.challenger.core.Contributions;
 import de.slothsoft.tetris.contrib.ExampleStonePositioner;
 
 /**
  * Util class for getting and managing {@link StonePositioner}s (hence the name)
- * 
+ *
  * @since 1.0.0
  */
 
 public final class StonePositioners {
 
 	/**
-	 * Returns all instances of {@link StonePositioner}s in the
-	 * <code>contrib</code> package.
-	 * 
+	 * Returns all instances of {@link StonePositioner}s in the <code>contrib</code>
+	 * package.
+	 *
 	 * @return a list of stone positioners
 	 */
 
 	public static List<StonePositioner> getStonePositioners() {
-		return getStonePositioners(ExampleStonePositioner.class.getPackage());
-	}
-
-	/**
-	 * Returns all instances of {@link StonePositioner}s in a specified package.
-	 * 
-	 * @param searchedPackage
-	 *            - the package to be searched
-	 * @return a list of stone positioners
-	 */
-
-	static List<StonePositioner> getStonePositioners(Package searchedPackage) {
-		try {
-			List<StonePositioner> stonePositioner = new ArrayList<>();
-			List<Class<?>> classes = getClasses(searchedPackage.getName());
-			for (Class<?> clazz : classes) {
-				if (!Modifier.isAbstract(clazz.getModifiers()) && StonePositioner.class.isAssignableFrom(clazz)) {
-					stonePositioner.add((StonePositioner) clazz.newInstance());
-				}
-			}
-			return stonePositioner;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	/**
-	 * Returns all implementations of {@link StonePositioner}s in a specified
-	 * package.
-	 * 
-	 * @param packageName
-	 *            - the package to be searched (e.g. "de.slothsoft.tetris")
-	 * @return a list of classes
-	 */
-
-	static List<Class<?>> getClasses(String packageName) throws ClassNotFoundException, IOException {
-		List<Class<?>> classes = new ArrayList<>();
-		String path = packageName.replace('.', '/');
-
-		try {
-			URL resource = StonePositioners.class.getResource('/' + path);
-			if (resource == null) return classes;
-			URI uri = resource.toURI();
-
-			Path[] pathToFolders;
-			if (uri.getScheme().equals("jar")) {
-				// we are in the application JAR
-				FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object> emptyMap());
-				pathToFolders = new Path[] { fileSystem.getPath('/' + path) };
-			} else {
-				// we are in the IDE
-				ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-				List<URL> resources = Collections.list(classLoader.getResources(path));
-				pathToFolders = new Path[resources.size()];
-				for (int i = 0; i < pathToFolders.length; i++) {
-					pathToFolders[i] = Paths.get(resources.get(i).toURI());
-				}
-			}
-			for (Path pathToFolder : pathToFolders)
-				classes.addAll(findClasses(pathToFolder, packageName));
-
-		} catch (URISyntaxException e) {
-			throw new RuntimeException("This really should not have happend o_O", e);
-		}
-		return classes;
-	}
-
-	private static List<Class<?>> findClasses(Path directory, String packageName)
-			throws ClassNotFoundException, IOException {
-		List<Class<?>> classes = new ArrayList<>();
-		try (Stream<Path> walker = Files.walk(directory, Integer.MAX_VALUE)) {
-			for (Iterator<Path> it = walker.iterator(); it.hasNext();) {
-				Path file = it.next();
-				if (!Files.isDirectory(file)) {
-					String fileName = file.getFileName().toString();
-					if (fileName.endsWith(".class")) {
-						classes.add(Class.forName(packageName + '.' + fileName.substring(0, fileName.length() - 6)));
-					}
-				}
-			}
-		}
-		return classes;
+		return Contributions.fetchImplementations(ExampleStonePositioner.class.getPackage(), StonePositioner.class);
 	}
 
 	private StonePositioners() {
--- src/test/java/de/slothsoft/tetris/StonePositionersTest.java
+++ src/test/java/de/slothsoft/tetris/StonePositionersTest.java
@@ -1,53 +1,17 @@
 package de.slothsoft.tetris;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import de.slothsoft.tetris.contrib.ExampleStonePositioner;
-import de.slothsoft.tetris.contrib1.A;
-import de.slothsoft.tetris.contrib1.B;
-import de.slothsoft.tetris.contrib1.C;
-import de.slothsoft.tetris.contrib2.Apple;
-import de.slothsoft.tetris.contrib2.Banana;
-import de.slothsoft.tetris.contrib2.Orange;
-import de.slothsoft.tetris.contrib2.Pear;
-import de.slothsoft.tetris.contrib3.One;
-import de.slothsoft.tetris.contrib3.Three;
-import de.slothsoft.tetris.contrib3.Two;
 
 public class StonePositionersTest {
 
 	@Test
-	public void testGetClasses() throws Exception {
-		List<Class<?>> result = StonePositioners.getClasses("de.slothsoft.tetris.contrib1");
-		Assert.assertEquals(Arrays.asList(A.class, B.class, C.class), result);
-	}
-
-	@Test
-	public void testGetClassesUnknownPackage() throws Exception {
-		List<Class<?>> result = StonePositioners.getClasses("de.slothsoft.unknown");
-		Assert.assertEquals(new ArrayList<>(), result);
-	}
-
-	@Test
-	public void testGetPositioners() throws Exception {
-		List<StonePositioner> result = StonePositioners.getStonePositioners(Apple.class.getPackage());
-		Assert.assertEquals(Arrays.asList(new Banana(), new Orange(), new Pear()), result);
-	}
-
-	@Test
-	public void testGetPositionersIgnoreAbstractClasses() throws Exception {
-		List<StonePositioner> result = StonePositioners.getStonePositioners(One.class.getPackage());
-		Assert.assertEquals(Arrays.asList(new Three(), new Two()), result);
-	}
-
-	@Test
 	public void testGetDefaultPositioners() throws Exception {
-		List<StonePositioner> result = StonePositioners.getStonePositioners();
+		final List<StonePositioner> result = StonePositioners.getStonePositioners();
 		Assert.assertTrue("The example is missing: " + result, result.contains(new ExampleStonePositioner()));
 	}
 
